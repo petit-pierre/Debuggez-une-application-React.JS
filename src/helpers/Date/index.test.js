6551c8a8fbd27d307bc67b4b52b032da4bfd8f02
petit-pierre <aubree.pierre2@gmail.com>
2023-12-28 18:45:20 +0100

    V0.9.1 test dates

--- src/helpers/Date/index.test.js
+++ src/helpers/Date/index.test.js
@@ -1,15 +1,14 @@
-import { expect } from "@testing-library/react";
 import { getMonth } from "./index";
 
 describe("Date helper", () => {
   describe("When getMonth is called", () => {
     it("the function return janvier for 2022-01-01 as date", () => {
-      const date = "2022-01-01";
-      const result = getMonth(date);
+      const result = getMonth(new Date("2022-01-01"));
       expect(result).toBe("janvier");
     });
     it("the function return juillet for 2022-07-08 as date", () => {
-      // to implement
+      const result = getMonth(new Date("2022-07-08"));
+      expect(result).toBe("juillet");
     });
   });
 });
